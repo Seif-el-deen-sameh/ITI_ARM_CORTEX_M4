ba61d3e34695dc9991b4593e7774fed91594e43f
Seif-el-deen-sameh <94634728+Seif-el-deen-sameh@users.noreply.github.com>
2023-05-11 11:41:55 +0300

    files

---
+++ src/MCAL/GPIO/GPIO_config.h
@@ -0,0 +1,13 @@
+/*
+ * GPIO_config.h
+ *
+ *  Created on: May 11, 2023
+ *      Author: ss210
+ */
+
+#ifndef MCAL_GPIO_GPIO_CONFIG_H_
+#define MCAL_GPIO_GPIO_CONFIG_H_
+
+
+
+#endif /* MCAL_GPIO_GPIO_CONFIG_H_ */
---
+++ src/MCAL/GPIO/GPIO_interface.h
@@ -0,0 +1,13 @@
+/*
+ * GPIO_interface.h
+ *
+ *  Created on: May 11, 2023
+ *      Author: ss210
+ */
+
+#ifndef MCAL_GPIO_GPIO_INTERFACE_H_
+#define MCAL_GPIO_GPIO_INTERFACE_H_
+
+
+
+#endif /* MCAL_GPIO_GPIO_INTERFACE_H_ */
