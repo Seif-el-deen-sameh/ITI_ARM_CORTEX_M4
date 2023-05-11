88fbc45942497502d5ff8b28dd15d3e8c9d6e41c
Seif-el-deen-sameh <94634728+Seif-el-deen-sameh@users.noreply.github.com>
2023-05-11 14:00:37 +0300

    files

--- src/MCAL/GPIO/GPIO_interface.h
+++ src/MCAL/GPIO/GPIO_interface.h
@@ -9,7 +9,23 @@
 #define MCAL_GPIO_GPIO_INTERFACE_H_
 #include "LIB/bit_math.h"
 #include "LIB/std_types.h"
-void GPIO_Init_Pin_Output(u8 port_id,u8 pin_id,u8 pin_mode,u8 pin_speed);
+/*turn to enums*/
+#define GPIO_PORTA	1
+#define GPIO_PORTB	2
+#define GPIO_PORTC	3
+/*turn to enums*/
+#define GPIO_PUSH_PULL	1
+#define GPIO_OPEN_DRAIN	2
+/*turn to enums*/
+#define GPIO_SPEED_LOW			1
+#define GPIO_SPEED_MED			2
+#define GPIO_SPEED_HIGH			3
+#define GPIO_SPEED_VERY_HIGH	4
+/*turn to enums*/
+#define GPIO_OUTPUT_HIGH	1
+#define GPIO_OUTPUT_LOW		0
+/*turn to enums*/
+void GPIO_Init_Pin_Output(u8 port_id,u8 pin_id,u8 pin_type,u8 pin_speed);
 void GPIO_Init_Pin_Input (u8 port_id,u8 pin_id,u8 pin_mode);
 void GPIO_Set_Pin_Value	 (u8 port_id,u8 pin_id,u8 pin_value);
 u8 GPIO_Get_Pin_Value 	 (u8 port_id,u8 pin_id);
--- src/MCAL/GPIO/GPIO_program.c
+++ src/MCAL/GPIO/GPIO_program.c
@@ -9,15 +9,84 @@
 #include "GPIO_config.h"
 #include "GPIO_interface.h"
 #include "GPIO_private.h"
-void GPIO_Init_Pin_Output(u8 port_id,u8 pin_id,u8 pin_mode,u8 pin_speed){
-
+void GPIO_Init_Pin_Output(u8 port_id,u8 pin_id,u8 pin_type,u8 pin_speed){
+	switch(port_id)
+		{
+				case GPIO_PORTA:
+					// PIN MODE BY MASKING
+				GPIOA->MODER   &=~(0b11<<(pin_id*2));
+				GPIOA->MODER   |=(0b01<<(pin_id*2));
+					//PIN SPEED BY MASKING
+				GPIOA->OSPEEDR &=~(0b11<<(pin_id*2));
+				GPIOA->OSPEEDR |=(pin_speed<<(pin_id*2));
+					// PIN TYPE
+				wrt_bit(GPIOA->OTYPER,pin_id,pin_type);
+				break;
+				case GPIO_PORTB:
+				GPIOB->MODER   &=~(0b11<<(pin_id*2));
+				GPIOB->MODER   |=(0b01<<(pin_id*2));
+				GPIOB->OSPEEDR &=~(0b11<<(pin_id*2));
+				GPIOB->OSPEEDR |=(pin_speed<<(pin_id*2));
+				wrt_bit(GPIOB->OTYPER,pin_id,pin_type);
+				break;
+				case GPIO_PORTC:
+				GPIOC->MODER   &=~(0b11<<(pin_id*2));
+				GPIOC->MODER   |=(0b01<<(pin_id*2));
+				GPIOC->OSPEEDR &=~(0b11<<(pin_id*2));
+				GPIOC->OSPEEDR |=(pin_speed<<(pin_id*2));
+				wrt_bit(GPIOC->OTYPER,pin_id,pin_type);
+				break;
+			}
+			break;
+		}
 }
 void GPIO_Init_Pin_Input (u8 port_id,u8 pin_id,u8 pin_mode){
+	switch(port_id)
+			{
+					case GPIO_PORTA:
 
+					break;
+					case GPIO_PORTB:
+
+					break;
+					case GPIO_PORTC:
+
+					break;
+				}
+				break;
+			}
 }
 void GPIO_Set_Pin_Value	 (u8 port_id,u8 pin_id,u8 pin_value){
+	switch(port_id)
+			{
+					case GPIO_PORTA:
 
+					break;
+					case GPIO_PORTB:
+
+					break;
+					case GPIO_PORTC:
+
+					break;
+				}
+				break;
+			}
 }
 u8 GPIO_Get_Pin_Value 	 (u8 port_id,u8 pin_id){
+	u8 val = 0;
+	switch(port_id)
+			{
+					case GPIO_PORTA:
 
+					break;
+					case GPIO_PORTB:
+
+					break;
+					case GPIO_PORTC:
+
+					break;
+				}
+				break;
+			}
+		return val;
 }
